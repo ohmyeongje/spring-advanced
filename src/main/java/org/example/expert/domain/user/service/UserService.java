cfe67fcb064164303cac4d693ae013e19b023907
myeongje <tiqmtiqm6541@gmail.com>
2025-02-25 17:28:09 +0900

    레벨 1-1 Early return 재수정

--- src/main/java/org/example/expert/domain/user/service/UserService.java
+++ src/main/java/org/example/expert/domain/user/service/UserService.java
@@ -25,15 +25,6 @@ public class UserService {
 
     @Transactional
     public void changePassword(long userId, UserChangePasswordRequest userChangePasswordRequest) {
-<<<<<<< HEAD
-
-=======
-        if (userChangePasswordRequest.getNewPassword().length() < 8 ||
-                !userChangePasswordRequest.getNewPassword().matches(".*\\d.*") ||
-                !userChangePasswordRequest.getNewPassword().matches(".*[A-Z].*")) {
-            throw new InvalidRequestException("새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다.");
-        }
->>>>>>> 23088f7498a938ab5d8c860020ed03866d19b621
 
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new InvalidRequestException("User not found"));
@@ -42,15 +33,12 @@ public class UserService {
             throw new InvalidRequestException("새 비밀번호는 기존 비밀번호와 같을 수 없습니다.");
         }
 
-<<<<<<< HEAD
         if (userChangePasswordRequest.getNewPassword().length() < 8 ||
                 !userChangePasswordRequest.getNewPassword().matches(".*\\d.*") ||
                 !userChangePasswordRequest.getNewPassword().matches(".*[A-Z].*")) {
             throw new InvalidRequestException("새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다.");
         }
 
-=======
->>>>>>> 23088f7498a938ab5d8c860020ed03866d19b621
         if (!passwordEncoder.matches(userChangePasswordRequest.getOldPassword(), user.getPassword())) {
             throw new InvalidRequestException("잘못된 비밀번호입니다.");
         }
